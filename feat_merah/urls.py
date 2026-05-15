907ed3cebd10437f7f05b84858973003c6838781
Nimaisya Gina <nimaisyagina@Nimaisyas-MacBook-Air.local>
2026-05-15 14:45:25 +0700

    trigger fix and logical fix

--- feat_merah/urls.py
+++ feat_merah/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
-    manage_rewards_view, manage_partners_view, member_redeem_view
+    manage_rewards_view, manage_partners_view, member_redeem_view,
+    approve_claim_missing_miles_view, laporan_transaksi_view
 )
 
 app_name = 'feat_merah'
@@ -9,4 +10,6 @@ urlpatterns = [
     path('manage-rewards/', manage_rewards_view, name='manage_rewards'),
     path('manage-partners/', manage_partners_view, name='manage_partners'),
     path('redeem/', member_redeem_view, name='member_redeem'),
+    path('approve-claims/', approve_claim_missing_miles_view, name='approve_claims'),
+    path('laporan-transaksi/', laporan_transaksi_view, name='laporan_transaksi'),
 ]
